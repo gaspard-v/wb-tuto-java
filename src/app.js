fdc899e0509caa81f7358d7588fd3db410ef9ca3
Gaspard <gaspard.vandecasteele@gmail.com>
2022-03-10 12:28:12 +0100

    add mui elements

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@ import Menu from './Menu';
 import Navigator from './Navigator';
 import { BrowserRouter } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import Grid from '@mui/material/Grid';
 
 export default function App() {
 	const [data, setData] = useState(null);
 	const [error, setError] = useState(null);
 	useEffect(() => {
-		fetchData();
+		setTimeout(fetchData, 5000);
 	}, []);
 
 	function fetchData() {
@@ -38,10 +47,36 @@ export default function App() {
 				</BrowserRouter>
 			);
 		} else if (error) {
-			return <p>{`Une erreur est survenu: ${error}`}</p>;
+			return (
+				<Alert
+					severity="error"
+					action={
+						<Button color="inherit" size="small" onClick={() => fetchData()}>
+							Réessayer
+						</Button>
+					}
+				>
+					<AlertTitle>Erreur !</AlertTitle>
+					Il y une erreur ! <strong>{error.message}</strong>
+				</Alert>
+			);
 		} else {
-			return <p>Chargement ...</p>;
+			return (
+				<Grid
+					container
+					spacing={0}
+					direction="column"
+					alignItems="center"
+					justifyContent="center"
+					style={{ minHeight: '100vh' }}
+				>
+					<Grid item xs={2}>
+						<CircularProgress />
+						<p>chargement...</p>
+					</Grid>
+				</Grid>
+			);
 		}
 	}
-	return displayApp();
+	return <Container maxWidth="">{displayApp()}</Container>;
 }
