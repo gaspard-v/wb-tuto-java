10eb1657f77fa0d9043c7d83dbc50e95f0286a31
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2022-04-12 14:24:50 +0200

    add menu to burger button

--- src/MenuItemsBurger.js
+++ src/MenuItemsBurger.js
@@ -1,15 +1,28 @@
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import List from '@mui/material/List';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 export default function MenuItemsBurger({ items }) {
+	const handleInconClick = () => {};
 	return (
-		<IconButton
-			size="large"
-			edge="start"
-			color="inherit"
-			aria-label="open drawer"
-			sx={{ mr: 2 }}
-		>
-			<MenuIcon />
-		</IconButton>
+		<div>
+			<List>
+				<IconButton
+					id="menu-button"
+					size="large"
+					edge="start"
+					color="inherit"
+					aria-label="open drawer"
+					onClick={handleInconClick}
+					sx={{ mr: 2 }}
+				>
+					<MenuIcon />
+				</IconButton>
+			</List>
+			<Menu open={false}>
+				<MenuItem>okeki</MenuItem>
+			</Menu>
+		</div>
 	);
 }
