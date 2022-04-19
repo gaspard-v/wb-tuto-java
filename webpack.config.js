f3f1791651c9f3130364821a661d84667a32a618
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2022-04-19 14:54:25 +0200

    delete old babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,62 @@
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const path = require('path');
 
-module.exports = (env, { mode }) => ({
-	// Fichier d'entrée :
-	entry: './src/index.js',
-	// Fichier de sortie :
-	output: {
-		path: path.resolve(__dirname, './build'),
-		filename: 'index.bundle.js',
-		// configuration du fast-refresh [1/3]
-		// pour que webpack serve publie le js dans un sous-dossier fictif build
-		publicPath: '/build/',
-	},
-	// compatibilité anciens navigateurs (si besoin du support de IE11 ou android 4.4)
-	target: ['web', 'es5'],
-	// connexion webpack <-> babel :
-	module: {
-		rules: [
-			{
-				test: /\.js$/, // tous les fichiers js ...
-				exclude: /node_modules/, // ... sauf le dossier node_modules ...
-				use: {
-					// ... seront compilés par babel !
-					loader: 'babel-loader',
-				},
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
+module.exports = (env, { mode }) => {
+	const bablerc = {
+		presets: [
+			'@babel/preset-env',
+			['@babel/preset-react', { runtime: 'automatic' }],
 		],
-	},
-	// configuration du fast refresh [2/3]
-	plugins: [mode === 'development' && new ReactRefreshWebpackPlugin()].filter(
-		Boolean
-	),
-	// configuration du fast-refresh [3/3]
-	devServer: {
-		// choix du port du serveur webpack qui va servir notre application
-		port: 8000,
-		// configuration du dossier racine du serveur
-		static: {
-			directory: './',
-			watch: false, // évite de recharger toute la page quand un fichier est modifié
+		plugins: [
+			mode == 'development' && require.resolve('react-refresh/babel'),
+		].filter(Boolean),
+	};
+	return {
+		// Fichier d'entrée :
+		entry: './src/index.js',
+		// Fichier de sortie :
+		output: {
+			path: path.resolve(__dirname, './build'),
+			filename: 'index.bundle.js',
+			// configuration du fast-refresh [1/3]
+			// pour que webpack serve publie le js dans un sous-dossier fictif build
+			publicPath: '/build/',
 		},
-	},
-	devtool: 'eval-cheap-module-source-map',
-});
+		// compatibilité anciens navigateurs (si besoin du support de IE11 ou android 4.4)
+		target: ['web', 'es5'],
+		// connexion webpack <-> babel :
+		module: {
+			rules: [
+				{
+					test: /\.js$/, // tous les fichiers js ...
+					exclude: /node_modules/, // ... sauf le dossier node_modules ...
+					use: {
+						// ... seront compilés par babel !
+						loader: 'babel-loader',
+						options: bablerc,
+					},
+				},
+				{
+					test: /\.css$/,
+					exclude: /node_modules/,
+					use: ['style-loader', 'css-loader'],
+				},
+			],
+		},
+		// configuration du fast refresh [2/3]
+		plugins: [mode === 'development' && new ReactRefreshWebpackPlugin()].filter(
+			Boolean
+		),
+		// configuration du fast-refresh [3/3]
+		devServer: {
+			// choix du port du serveur webpack qui va servir notre application
+			port: 8000,
+			// configuration du dossier racine du serveur
+			static: {
+				directory: './',
+				watch: false, // évite de recharger toute la page quand un fichier est modifié
+			},
+		},
+		devtool: 'eval-cheap-module-source-map',
+	};
+};
