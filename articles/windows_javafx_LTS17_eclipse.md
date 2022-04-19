d93ae448d6c3565f23c12b7e3025ceb259b09a8d
Gaspard VANDECASTEELE <gaspard.vandecasteele@student.yncrea.fr>
2022-04-19 19:48:48 +0200

    modif

---
+++ articles/windows_javafx_LTS17_eclipse.md
@@ -0,0 +1,17 @@
+# Création d'un executable à partir d'un projet JavaFX sous Windows
+
+## Introduction
+
+ce tutoriel a pour but de vous aider à créer un executable à partir d'un projet JavaFX sous Windows, via l'IDE Eclipse.
+ce document ce focalisera sur la version LTS 17 de Java, néanmoins la version 11 est aussi supportée.
+
+## Table des matières
+
+1. [Installation de l'environnement de développement](#installation-de-l-environnement-de-developpement)
+   1.1. [Installation de l'IDE Eclipse](#installation-de-l-ide-eclipse)
+   1.2. [Installation de Java](#installation-de-java)
+   1.3. [Installation de JavaFX](#installation-de-javafx)
+
+## Installation de l'environnement de développement {#installation-de-l-environnement-de-developpement}
+
+test
--- src/ArticlesList.js
+++ src/ArticlesList.js
@@ -1,23 +1,50 @@
+import * as React from 'react';
 import Link from '@mui/material/Link';
 import { NavLink } from 'react-router-dom';
 import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
 
 export default function ArticlesList({ data }) {
 	return (
 		<>
 			{data.map(
-				({ description, id, options: { hidden } }) =>
-					hidden || (
-						<Link
-							key={id}
-							component={NavLink}
-							to={`/article/${id}`}
-							color="inherit"
-							sx={{ mr: 2, color: 'black', display: 'block' }}
-						>
-							{description}
-						</Link>
-					)
+				// ({ description, id, options: { hidden } }) =>
+				// 	hidden || (
+				// 		<Link
+				// 			key={id}
+				// 			component={NavLink}
+				// 			to={`/article/${id}`}
+				// 			color="inherit"
+				// 			sx={{ mr: 2, color: 'black', display: 'block' }}
+				// 		>
+				// 			{description}
+				// 		</Link>
+				// 	)
+				<List
+					sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+				>
+					<ListItem alignItems="flex-start">
+						<ListItemText
+							primary="Brunch this weekend?"
+							secondary={
+								<React.Fragment>
+									<Typography
+										sx={{ display: 'inline' }}
+										component="span"
+										variant="body2"
+										color="text.primary"
+									>
+										Ali Connors
+									</Typography>
+									{" — I'll be in your neighborhood doing errands this…"}
+								</React.Fragment>
+							}
+						/>
+					</ListItem>
+				</List>
 			)}
 		</>
 	);
